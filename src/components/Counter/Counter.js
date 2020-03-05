4d7c2e7332759c1577dd09ad8d05fb7a01fa778b
joelraju <joelrajusomala@github.com>
2020-03-05 16:56:14 +0530

    updated

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -11,30 +11,30 @@ class Counter extends Component {
         }
     }
 
-
     subCount = () => {
-        if(this.count !== 0)
-        this.setState({
-            count: this.state.count -1
-        })
+        this.setState((preState) => ({
+            count: preState.count - 1
+        }))
     }
 
     addCount = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
+        this.setState((preState) => ({
+            count: preState.count + 1
+        }))
     }
 
-    
-
-
     render() {
-        
         return (
             <div id='counter'>
-                <button className="btn" disabled={this.state.count ===0} onClick={this.subCount}>-</button>
+                <button
+                    className="btn"
+                    disabled={this.state.count === 0}
+                    onClick={this.subCount}>-</button>
                 <p>{this.state.count}</p>
-                <button className="btn" disabled={this.state.count ===100} onClick={this.addCount}>+</button>
+                <button
+                    className="btn"
+                    disabled={this.state.count === 100}
+                    onClick={this.addCount}>+</button>
             </div>
         )
     }
